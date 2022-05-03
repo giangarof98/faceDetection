350d6fc9670940648ca951c73a01c44b4c9bc449
Gianmarco G <giangarof98@gmail.com>
2022-05-03 15:35:24 -0400

    safari webcam

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-const video = document.getElementById('video');
+// const video = document.getElementById('video');
+var constraints = { video: { width: 1280, height: 720 } };
 
-function startVideo(){
-    navigator.getUserMedia(
-        {video: {} },
-        stream => video.srcObject = stream,
-        err => console.error(err)
-    )
-}
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
+    // faceapi.nets.faceLandMark68Net.loadFromUri('./models'),
+    // faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
+    // faceapi.nets.faceExpressionNet.loadFromUri('./models'),
+]);
 
-startVideo()
+// function startVideo(){
+//     navigator.mediaDevices.getUserMedia(constraints,
+//         stream => video.srcObject = stream,
+//         err => console.error(err)
+//     )
+// }
+// startVideo()
+
+navigator.mediaDevices.getUserMedia(constraints)
+.then(function(mediaStream) {
+    var video = document.getElementById('video');
+    video.srcObject = mediaStream;
+    video.onloadedmetadata = function(e) {
+      video.play();
+    };
+  })
+.catch(function(err) { console.log(err.name + ": " + err.message); })
