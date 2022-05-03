dd2fa78b959b4897a85c40959f96ebaeef7c1490
Gianmarco G <giangarof98@gmail.com>
2022-05-03 16:22:30 -0400

    recognition

--- app.js
+++ app.js
@@ -1,27 +1,48 @@
-// const video = document.getElementById('video');
-var constraints = { video: { width: 1280, height: 720 } };
+const video = document.getElementById('video');
+// var constraints = { video: { width: 1280, height: 720 } };
 
 Promise.all([
     faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
-    // faceapi.nets.faceLandMark68Net.loadFromUri('./models'),
-    // faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
-    // faceapi.nets.faceExpressionNet.loadFromUri('./models'),
-]);
+    faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('./models'),
+]).then(startVideo)
 
-// function startVideo(){
-//     navigator.mediaDevices.getUserMedia(constraints,
-//         stream => video.srcObject = stream,
-//         err => console.error(err)
-//     )
-// }
-// startVideo()
+function startVideo(){
+    navigator.getUserMedia(
+        {video: {} },
+        stream => video.srcObject = stream,
+        err => console.error(err)
+    )
+}
+startVideo();
 
-navigator.mediaDevices.getUserMedia(constraints)
-.then(function(mediaStream) {
-    var video = document.getElementById('video');
-    video.srcObject = mediaStream;
-    video.onloadedmetadata = function(e) {
-      video.play();
-    };
-  })
-.catch(function(err) { console.log(err.name + ": " + err.message); })
+video.addEventListener('play', () => {
+    const canvas = faceapi.createCanvasFromMedia(video);
+    document.body.append(canvas);
+    const displaySize = { width: video.width, height: video.height};
+    faceapi.matchDimensions(canvas, displaySize)
+    setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(video,
+            new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+        const resize = faceapi.resizeResults(detections, displaySize);
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+        faceapi.draw.drawDetections(canvas, resize)
+        //faceapi.draw.drawFaceLandmarks(canvas, resize)
+        faceapi.draw.drawFaceExpressions(canvas, resize)
+    }, 100)
+    
+})
+
+
+//safari
+
+// navigator.mediaDevices.getUserMedia(constraints)
+// .then(function(mediaStream) {
+//     var video = document.getElementById('video');
+//     video.srcObject = mediaStream;
+//     video.onloadedmetadata = function(e) {
+//       video.play();
+//     };
+//   })
+// .catch(function(err) { console.log(err.name + ": " + err.message); })
